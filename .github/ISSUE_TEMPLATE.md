542ab93a9ff1beba8f99f53266bb11a1035c784f
Shawn Bruce <kantlivelong@gmail.com>
2021-04-24 23:51:44 -0400

    Initial Commit

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,95 @@
+<!--
+READ THE FOLLOWING FIRST:
+
+If not already done, please read the Wiki: 
+https://github.com/kantlivelong/OctoPrint-PSUControl/wiki
+https://github.com/kantlivelong/OctoPrint-PSUControl-Tasmota/wiki
+
+This is a bug tracker, please only use it to report bugs
+within OctoPrint-PSUControl-Tasmota.
+
+Do not seek support here ("I need help with ...", "I have a
+question ..."), that belongs on the community forum at 
+https://community.octoprint.org
+All support related questions will be closed.
+
+Feature requests should be prefixed with [REQUEST]
+
+When reporting a bug do NOT delete ANY lines from the template or exclude 
+any information unless otherwise noted.
+Failure to follow this will result in the ticket being closed and locked.
+
+Make sure any bug you want to report is still present with the CURRENT
+OctoPrint-PSUControl-Tasmota version.
+
+To summarize:
+Wiki:             https://github.com/kantlivelong/OctoPrint-PSUControl/wiki
+                  https://github.com/kantlivelong/OctoPrint-PSUControl-Tasmota/wiki
+Support:          https://community.octoprint.org
+Bug Reports:      Here
+Feature Requests: Here
+Thanks!
+-->
+
+#### What were you doing?
+
+<!-- 
+Please be as specific as possible here. The maintainers will need to
+reproduce your issue in order to fix it and that is not possible if they
+don't know what you did to get it to happen in the first place.
+
+Ideally provide exact steps to follow in order to reproduce your problem:
+-->
+
+1. ...
+2. ...
+3. ...
+
+<!--
+If you encountered a problem with specific files of any sorts, make sure
+to also include a link to a file with which to reproduce the problem.
+-->
+
+#### What did you expect to happen?
+
+#### What happened instead?
+
+#### Version of OctoPrint-PSUControl
+
+<!--
+Can be found in Plugin Manager. ALWAYS INCLUDE.
+-->
+
+#### Version of OctoPrint-PSUControl-Tasmota
+
+<!--
+Can be found in Plugin Manager. ALWAYS INCLUDE.
+-->
+
+#### Operating System running OctoPrint
+
+<!--
+OctoPi, Linux, Windows, MacOS, something else? With version please.
+OctoPi's version can be found in /etc/octopi_version or in the lower left
+corner of the web interface.
+-->
+
+#### Printer model & used firmware incl. version
+
+<!--
+If applicable, always include if unsure.
+-->
+
+
+#### Attach or link DEBUG log files.
+
+<!--
+Details on how to capture logs can be found at:
+https://github.com/kantlivelong/OctoPrint-PSUControl/wiki/Troubleshooting#how-to-provide-logs
+
+Pastebins:
+https://gist.github.com
+https://pastebin.com
+
+ALWAYS INCLUDE and never truncate.
+-->
---
+++ octoprint_psucontrol_tasmota/__init__.py
@@ -0,0 +1,176 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+__author__ = "Shawn Bruce <kantlivelong@gmail.com>"
+__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
+__copyright__ = "Copyright (C) 2021 Shawn Bruce - Released under terms of the AGPLv3 License"
+
+import octoprint.plugin
+import requests
+
+class PSUControl_Tasmota(octoprint.plugin.StartupPlugin,
+                         octoprint.plugin.RestartNeedingPlugin,
+                         octoprint.plugin.TemplatePlugin,
+                         octoprint.plugin.SettingsPlugin):
+
+    def __init__(self):
+        self.config = dict()
+
+
+    def get_settings_defaults(self):
+        return dict(
+            address = '',
+            plug = 1,
+            enable_auth = False,
+            username = 'admin',
+            password = ''
+        )
+
+
+    def on_settings_initialized(self):
+        self.reload_settings()
+
+
+    def reload_settings(self):
+        for k, v in self.get_settings_defaults().items():
+            if type(v) == str:
+                v = self._settings.get([k])
+            elif type(v) == int:
+                v = self._settings.get_int([k])
+            elif type(v) == float:
+                v = self._settings.get_float([k])
+            elif type(v) == bool:
+                v = self._settings.get_boolean([k])
+
+            self.config[k] = v
+            self._logger.debug("{}: {}".format(k, v))
+
+
+    def on_startup(self, host, port):
+        psucontrol_helpers = self._plugin_manager.get_helpers("psucontrol")
+        if 'register_plugin' not in psucontrol_helpers.keys():
+            self._logger.warning("The version of PSUControl that is installed does not support plugin registration.")
+            return
+
+        self._logger.debug("Registering plugin with PSUControl")
+        psucontrol_helpers['register_plugin'](self)
+
+
+    def send(self, cmd):
+        url = "http://{}/cm".format(self.config['address'])
+
+        params = dict(cmnd=cmd, timeout=5)
+
+        if self.config['enable_auth']:
+            params['user'] = self.config['username']
+            params['password'] = self.config['password']
+
+        response = None
+        try:
+            response = requests.get(url, params=params)
+        except (
+                requests.exceptions.InvalidURL,
+                requests.exceptions.ConnectionError
+        ):
+            self._logger.error("Unable to communicate with device. Check settings.")
+        except Exception:
+            self._logger.exception("Exception while making API call")
+        else:
+            self._logger.debug("cmd={}, status_code={}, text={}".format(cmd, response.status_code, response.text))
+
+            if response.status_code == 401:
+                self._logger.warning("Server returned 401 Unauthorized. Check credentials.")
+                response = None
+
+        return response
+
+
+    def change_psu_state(self, state):
+        cmd = "Power{} {}".format(self.config['plug'], state)
+        self.send(cmd)
+
+
+    def turn_psu_on(self):
+        self._logger.debug("Switching PSU On")
+        self.change_psu_state('on')
+
+
+    def turn_psu_off(self):
+        self._logger.debug("Switching PSU Off")
+        self.change_psu_state('off')
+
+
+    def get_psu_state(self):
+        cmd = "Status 0"
+
+        response = self.send(cmd)
+        if not response:
+            return False
+        data = response.json()
+
+        status = None
+        try:
+            status = (data['StatusSTS']['POWER' + str(self.config['plug'])] == 'ON')
+        except KeyError:
+            pass
+
+        if status == None and self.config['plug'] == 1:
+            try:
+                status = (data['StatusSTS']['POWER'] == 'ON')
+            except KeyError:
+                pass
+
+        if status == None:
+            self._logger.error("Unable to determine status. Check settings.")
+            status = False
+
+        return status
+
+
+    def on_settings_save(self, data):
+        octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+        self.reload_settings()
+
+
+    def get_settings_version(self):
+        return 1
+
+
+    def on_settings_migrate(self, target, current=None):
+        pass
+
+
+    def get_template_configs(self):
+        return [
+            dict(type="settings", custom_bindings=False)
+        ]
+
+
+    def get_update_information(self):
+        return dict(
+            psucontrol_tasmota=dict(
+                displayName="PSU Control - Tasmota",
+                displayVersion=self._plugin_version,
+
+                # version check: github repository
+                type="github_release",
+                user="kantlivelong",
+                repo="OctoPrint-PSUControl-Tasmota",
+                current=self._plugin_version,
+
+                # update method: pip w/ dependency links
+                pip="https://github.com/kantlivelong/OctoPrint-PSUControl-Tasmota/archive/{target_version}.zip"
+            )
+        )
+
+__plugin_name__ = "PSU Control - Tasmota"
+__plugin_pythoncompat__ = ">=2.7,<4"
+
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = PSUControl_Tasmota()
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
