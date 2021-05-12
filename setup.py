f2ceb7d0d134db619f53a5d080b4cf88e9495fc8
Shawn Bruce <kantlivelong@gmail.com>
2021-05-12 10:02:36 -0400

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 plugin_identifier = "psucontrol_tasmota"
 plugin_package = "octoprint_%s" % plugin_identifier
 plugin_name = "OctoPrint-PSUControl-Tasmota"
-plugin_version = "1.0.0"
+plugin_version = "1.0.1"
 plugin_description = "Adds Tasmota Smart Plug support to OctoPrint-PSUControl as a sub-plugin"
 plugin_author = "Shawn Bruce"
 plugin_author_email = "kantlivelong@gmail.com"
